4baef35f610d6a816c0a706d04aafca1a8105b5e
LeomirDias <leomirdiasti@outlook.com>
2025-11-14 12:08:04 -0300

    Refactor: Simplify layout component by removing PendingTicketAlert and improving code formatting for better readability.

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -1,17 +1,12 @@
-import { SidebarProvider } from "@/components/ui/sidebar"
+import { SidebarProvider } from "@/components/ui/sidebar";
 
-import { AppSidebar } from "./_components/app-sidebar"
-import PendingTicketAlert from "./_components/pending-ticket-alert"
-
+import { AppSidebar } from "./_components/app-sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-    return (
-        <SidebarProvider>
-            <AppSidebar />
-            <main className="w-full">
-                <PendingTicketAlert />
-                {children}
-            </main>
-        </SidebarProvider>
-    )
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main className="w-full">{children}</main>
+    </SidebarProvider>
+  );
 }
